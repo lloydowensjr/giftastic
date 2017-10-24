b149522d577640de649cfccf9117c293aba92ef4
klean.goods <klean.goods@Matts-MacBook-Pro.local>
2017-10-23 23:28:27 -0500

    initial pseudocoding done

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// start with an array of "topics"
+// using "topics" create buttons
+// buttons should dynamically popluate page
+// use a loop to display buttons
+// use the "static" link to display static gifs
+// an intial click of gif should animate gif
+// once clicked second time gif should go back to static state
+// create form field "input"
+// upon click of submit button
+// user's input should add "topic" to initial array 
+// user's input should create a new button that is placed on page
+
